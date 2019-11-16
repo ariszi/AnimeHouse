b195268e9109d79c864a2d26c6f869f683c562fe
aristofaniszisis <aristofanis.zisis@gmail.com>
2019-11-16 15:48:08 +0200

    Feature: Create a main activity

---
+++ app/src/main/java/zisis/aristofanis/animehouse/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package zisis.aristofanis.animehouse.presentation
+
+import AnimeListQuery
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import zisis.aristofanis.animehouse.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
