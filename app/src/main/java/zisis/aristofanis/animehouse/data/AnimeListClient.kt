ed520bdb1d139f16529d31e77f2c21845c5ff5bd
aristofaniszisis <aristofanis.zisis@gmail.com>
2019-11-17 22:20:55 +0200

    Feature: Create anime list client that is making the query to server
    This client is using apollo to query the base url. It awaits for result.
    At result the sealed class is used to save the response

---
+++ app/src/main/java/zisis/aristofanis/animehouse/data/AnimeListClient.kt
@@ -0,0 +1,25 @@
+package zisis.aristofanis.animehouse.data
+
+import AnimeListQuery
+import com.apollographql.apollo.api.Error
+import com.apollographql.apollo.coroutines.toDeferred
+import zisis.aristofanis.animehouse.data.mappers.AnimeListWithInfoMapper
+import zisis.aristofanis.animehouse.domain.QueryData
+import java.lang.Exception
+
+class AnimeListClient(private val query: AnimeListQuery) {
+
+    suspend fun getAnimeList(): QueryData {
+        try {
+            val response = RestProvider.client.query(query).toDeferred().await()
+            if (response.hasErrors()) {
+                return QueryData.Error(response.errors()[0])
+            }
+            return QueryData.Success(AnimeListWithInfoMapper().transform(response.data()))
+        } catch (e: Exception) {
+            return QueryData.Error(Error("Generic Error", listOf(), mapOf()))
+        }
+
+    }
+
+}
