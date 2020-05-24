5cd2233719e743fcaf5b1b4c8458d5cd6366b2aa
aristofaniszisis <aristofanis.zisis@gmail.com>
2020-05-24 20:51:06 +0300

    Refactor: Replace values with constant utils

--- app/src/main/java/zisis/aristofanis/animehouse/data/network/NetworkUtils.kt
+++ app/src/main/java/zisis/aristofanis/animehouse/data/network/NetworkUtils.kt
@@ -1,12 +1,13 @@
 package zisis.aristofanis.animehouse.data.network
 
 import zisis.aristofanis.animehouse.domain.models.QueryData
+import zisis.aristofanis.animehouse.domain.utils.EMPTY
 import com.apollographql.apollo.api.Error as ApolloError
 
 inline fun <Domain> result(codeBlock: () -> Domain): QueryData<Domain> =
     try {
         QueryData.Success(codeBlock())
     } catch (e: Exception) {
-        QueryData.Error(ApolloError(e.message, listOf(), mapOf()))
+        QueryData.Error(ApolloError(e.localizedMessage?: EMPTY, listOf(), mapOf()))
     }
 
