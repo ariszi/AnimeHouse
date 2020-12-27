046ab94934c075d316724ea96322aab6d0dcc53e
aristofaniszisis <aristofanis.zisis@gmail.com>
2020-12-27 23:55:31 +0200

    Clean up: Clean base use case

--- app/src/main/java/zisis/aristofanis/animehouse/domain/usecases/UseCase.kt
+++ app/src/main/java/zisis/aristofanis/animehouse/domain/usecases/UseCase.kt
@@ -1,28 +1,11 @@
 package zisis.aristofanis.animehouse.domain.usecases
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.flow.Flow
 
 abstract class UseCase<T, Params> {
 
-    private val useCaseScope: CoroutineScope = useCaseScope()
+    abstract suspend fun call(params: Params): Flow<T>
 
-    abstract suspend fun call(params: Params): T
+    suspend operator fun invoke(params: Params): Flow<T> = call(params)
 
-    suspend operator fun invoke(params: Params): T {
-        return call(params)
-    }
-
-    internal class UseCaseScope(context: CoroutineContext) : CoroutineScope {
-        override val coroutineContext: CoroutineContext = context
-    }
-
-    private fun useCaseScope() = UseCaseScope(SupervisorJob() + Dispatchers.Main)
-
-    fun unregister() {
-        useCaseScope.cancel()
-    }
 }
